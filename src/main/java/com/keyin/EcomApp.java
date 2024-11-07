ffa6bf489f7f13b36e37539b0d773b5340c52050
Jordan <jordan.kelloway@keyin.com>
2024-11-07 16:45:32 -0330

    Initial commit

--- src/main/java/com/keyin/EcomApp.java
+++ src/main/java/com/keyin/EcomApp.java
@@ -1,2 +1,14 @@
-package com.keyin;public class EcomApp {
+package com.keyin;
+
+import com.keyin.cars.CarService;
+
+import java.sql.SQLException;
+
+public class EcomApp {
+   private static final CarService carService = new CarService();
+    public static void main(String[] args) throws SQLException {
+
+
+        carService.listAllProducts();
+    }
 }
--- src/main/java/com/keyin/database/DatabaseConnection.java
+++ src/main/java/com/keyin/database/DatabaseConnection.java
@@ -1,2 +1,17 @@
-package com.keyin.database;public class DatabaseConnection {
+package com.keyin.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/ecomapplication2";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "jordan1234";
+
+    public static Connection getConnection() throws SQLException, SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+
 }
